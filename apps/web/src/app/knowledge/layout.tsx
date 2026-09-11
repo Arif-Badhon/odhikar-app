def1c6f43bd9547b22c5eecc95d45a71bdaec7d4
Arif <arifuzzaman.badhon1@gmail.com>
2026-09-12 00:32:16 +0600

    Fix layout and Supabase build keys

--- apps/web/src/app/knowledge/layout.tsx
+++ apps/web/src/app/knowledge/layout.tsx
@@ -1,4 +1,8 @@
 "use client";
 
 import Link from "next/link";
+import React from "react";
 
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return <>{children}</>;
+}
