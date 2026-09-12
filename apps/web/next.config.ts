ab632ac27bf76de9837904d2a4e69018cf709ffc
Arif <arifuzzaman.badhon1@gmail.com>
2026-09-12 16:30:56 +0600

    Updated api proxy

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -9,6 +9,14 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  async rewrites() {
+    return [
+      {
+        source: "/api_proxy/:path*",
+        destination: "http://api:8000/:path*", // Proxy to Backend
+      },
+    ];
+  },
 };
 
 export default nextConfig;
